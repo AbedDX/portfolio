1b1e4cc2f6137920ab079b85bf24be137c9df16c
Abed <abbe_92@live.com>
2025-03-15 04:18:46 +0200

    remove unnecessary famicons and updated package.json

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js?$/
-  }
+  base: '/portfolio/'
 })
